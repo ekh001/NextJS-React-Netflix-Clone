f05b2b96bdfe3d67646f2e829dae37c9c9d954b9
Liz Hall <lizk.hall@gmail.com>
2023-06-25 13:22:48 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main
+      className=''
+    >
+      <h1 className="text-green-300">Netflix Clone</h1>
+    </main>
+  )
+}
---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <div>
+      Hello my little testers
+    </div>
+  )
+}
+
+export default test
